2d24f6da1a242157ce57fea9945b66472a246a97
iamjoker021 <iamjoker021@gmail.com>
2024-07-04 17:51:16 +0530

    Create Re-Usable Grid create Function
    Refactored old code and move to re-usable funcation to createGrid

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// Add Div grid to Div Container
+// Create Grid of N Size
+const createGridOfNSize = (noOfSquares) => {
+    // Get Parent Container
+    const squareContainer = document.querySelector('div.squares');
 
-const squareContainer = document.querySelector('div.squares')
+    // Remove Grid
+    const grids = squareContainer.children;
+    for (const square of grids) {
+        squareContainer.removeChild(square);
+    }
 
-let noOfSquares = 16;
-for (let col = 0; col < noOfSquares; col++) {
-    for (let row = 0; row < noOfSquares; row++) {
-        const newSquare = document.createElement('div')
-        newSquare.classList.add('square');
-        newSquare.style["min-width"] = `${100/noOfSquares}%`
-        squareContainer.appendChild(newSquare);
+    // Create Grid inside Parent
+    for (let col = 0; col < noOfSquares; col++) {
+        for (let row = 0; row < noOfSquares; row++) {
+            const newSquare = document.createElement('div');
+            newSquare.classList.add('square');
+            newSquare.style["min-width"] = `${100/noOfSquares}%`;
+            squareContainer.appendChild(newSquare);
+        }
     }
 }
+
+// Create Intial Grid
+let noOfSquares = 16;
+createGridOfNSize(16);
